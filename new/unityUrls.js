459f881b827d70f9993213a60700d5455f2460e2
eMemcat <156375887+eMemcat@users.noreply.github.com>
2024-02-08 16:55:58 -0800

    Add files via upload

---
+++ new/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getBuildUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/238/7e828a918ebd488bb3693978bab968cc";
+}
+
+function getGameLoaderUrl() {
+	return getBuildUrl() + "/WebGL.loader.js";
+}
